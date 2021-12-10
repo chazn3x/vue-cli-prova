4d7e1bf7f2f58dd5ece59f1f6a9dbe77e2185cc4
Giovanni <gio.a.98@gmail.com>
2021-12-10 13:14:13 +0100

    prova cli

---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- parte html -->
+        <h2>Ciao Classe 47</h2>
+    </div>
+</template>
+
+<script>
+// parte logica
+export default {
+    name: 'Title'
+}
+</script>
+
+<style scoped>
+/* parte di stile */
+    h2 {
+        color: pink;
+    }
+</style>
